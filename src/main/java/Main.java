f1b1e20a8b78cc61b78f7e45ccc151b9e84698f7
Joodith <joodithl@gmail.com>
2023-09-04 12:12:08 +0530

    pass 2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,47 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
+        //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            while (true) {
+                clientSocket = serverSocket.accept();
+                System.out.println("Client connected from "+clientSocket.getInetAddress().getHostAddress());
+                BufferedReader in=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out=new PrintWriter(clientSocket.getOutputStream(),true);
+                String con_msg=in.readLine();
+                if(con_msg!=null && (con_msg.contains("PING")||con_msg.contains("Ping"))){
+                    System.out.println("Received message: "+con_msg);
+                    String resp="+PONG\\r\\n";
+                    out.println(resp);
+                    System.out.println("Sent response: "+resp);
+                }
+                
             }
-          } catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-          }
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-  }
+    }
 }
